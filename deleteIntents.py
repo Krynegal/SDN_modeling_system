265b1c81a09e8712c4fc2c1187581c526767c2b6
Krynegal <andrey.senyakin@gmail.com>
2022-05-09 00:27:52 +0300

    error handlers were added

--- deleteIntents.py
+++ deleteIntents.py
@@ -1,114 +1,42 @@
+import sys
+
 import requests as req
 import json
+
+import requests.exceptions
 
 import main
 
 USER = ("onos", "rocks")
 
 def get_intents():
-    res = req.get(f"http://{IP}:8181/onos/v1/intents?detail=true", auth=USER)
-    intents = res.json()["intents"]
-    print(json.dumps(res.json(), indent=4))
-    return intents
+    try:
+        res = req.get(f"http://{IP}:8181/onos/v1/intents?detail=true", auth=USER)
+        intents = res.json()["intents"]
+        # print(json.dumps(res.json(), indent=4))
+        return intents
+    except requests.exceptions.ConnectionError:
+        print("Oops. Seems like dns lookup failed..")
+        sys.exit()
 
 def delete_all_intents(intents):
+    successful_requests = 0
+    intents_num = len(intents)
     for intent in intents:
-        appId = intent["appId"]
-        key = intent["key"]
+        appId, key = intent["appId"], intent["key"]
         res = req.delete(f"http://{IP}:8181/onos/v1/intents/{appId}/{key}", auth=USER)
-        print(res)
-
-def update_intents(intents):
-    for intent in intents:
-        if intent["ingressPoint"]["device"] in ["of:0000000000000003", "of:0000000000000006"]:
-            appId = intent["appId"]
-            key = intent["key"]
-            res = req.delete(f"http://{IP}:8181/onos/v1/intents/{appId}/{key}", auth=USER)
-            print(res)
-    intents = {
-        "intents": [{
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "3",
-                "device": "of:0000000000000003"
-            },
-            "egressPoint": {
-                "port": "4",
-                "device": "of:0000000000000003"
-            }
-        }, {
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "4",
-                "device": "of:0000000000000003"
-            },
-            "egressPoint": {
-                "port": "3",
-                "device": "of:0000000000000003"
-            }
-        }, {
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "3",
-                "device": "of:0000000000000005"
-            },
-            "egressPoint": {
-                "port": "5",
-                "device": "of:0000000000000005"
-            }
-        }, {
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "5",
-                "device": "of:0000000000000005"
-            },
-            "egressPoint": {
-                "port": "3",
-                "device": "of:0000000000000005"
-            }
-        }, {
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "1",
-                "device": "of:0000000000000006"
-            },
-            "egressPoint": {
-                "port": "3",
-                "device": "of:0000000000000006"
-            }
-        }, {
-            "type": "PointToPointIntent",
-            "appId": "org.onosproject.cli",
-            "priority": 100,
-            "ingressPoint": {
-                "port": "3",
-                "device": "of:0000000000000006"
-            },
-            "egressPoint": {
-                "port": "1",
-                "device": "of:0000000000000006"
-            }
-        }]
-    }
-    for intent in intents["intents"]:
-        res = req.post(f"http://{IP}:8181/onos/v1/intents", json=intent, auth=USER)
-        print(res)
+        if res.status_code == 204:
+            successful_requests += 1
+    if successful_requests == intents_num:
+        print(f"{successful_requests}/{intents_num} were successful deleted")
+    else:
+        print(f"Oops. Only {successful_requests}/{intents_num} were successful deleted")
+        clear()
 
 def clear():
     global IP
     IP = main.get_ip()
     intents = get_intents()
-    # update_intents(intents)
     delete_all_intents(intents)
 
 if __name__ == '__main__':
